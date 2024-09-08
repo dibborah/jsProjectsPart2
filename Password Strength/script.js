f540c7cbec90654a081357e1bf4bb9c135d44e4a
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-08 20:46:15 +0530

    I completed rought the weak, medium and strong password funcitionality by using the length of strings of the password characters

--- Password Strength/script.js
+++ Password Strength/script.js
@@ -5,7 +5,17 @@ const strength = document.getElementById('strength');
 password.addEventListener('input', () => {
     if(password.value.length > 0){
         message.style.display = 'block'
+        strength.innerText = 'Weak';
+        strength.style.color = 'red';
     } else {
         message.style.display = 'none'
+    }
+    if(password.value.length >= 5) {
+        strength.innerText = 'Medium';
+        strength.style.color = 'orange';
+    }
+    if(password.value.length >= 8) {
+        strength.innerText = 'Strong';
+        strength.style.color = 'green';
     }
 });

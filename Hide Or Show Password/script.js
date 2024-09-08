47250e20fa7a934ea09f4c0646ef74b8d97cd992
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-08 20:03:53 +0530

    Done eye icon toggle funcitionality by adding and removing slash class

--- Hide Or Show Password/script.js
+++ Hide Or Show Password/script.js
@@ -4,7 +4,11 @@ const eyeicon = document.getElementById('eyeicon');
 eyeicon.onclick = () => {
     if (password.type === 'password') {
         password.type = 'text';
+        eyeicon.classList.add('fa-eye');
+        eyeicon.classList.remove('fa-eye-slash');
     } else {
         password.type = 'password';
+        eyeicon.classList.add('fa-eye-slash');
+        eyeicon.classList.remove('fa-eye');
     }
 }

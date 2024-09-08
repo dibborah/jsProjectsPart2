1d3e9d5c3ce54c723071c3852a33bcf4c32c591e
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-08 15:58:41 +0530

    Different toast message are having different colors

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -4,4 +4,10 @@ const showToast = (msg) => {
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.append(toast);
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
 };

25af4f05a86c7bf63b28aeb5c8ac48aeec84e9fe
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-08 20:50:12 +0530

    Completed the show password strength feature

--- Password Strength/script.js
+++ Password Strength/script.js
@@ -5,17 +5,20 @@ const strength = document.getElementById('strength');
 password.addEventListener('input', () => {
     if(password.value.length > 0){
         message.style.display = 'block'
-        strength.innerText = 'Weak';
+        strength.innerText = 'weak';
         strength.style.color = 'red';
+        password.style.borderColor = 'red';
     } else {
         message.style.display = 'none'
     }
     if(password.value.length >= 5) {
-        strength.innerText = 'Medium';
+        strength.innerText = 'medium';
         strength.style.color = 'orange';
+        password.style.borderColor = 'orange';
     }
     if(password.value.length >= 8) {
-        strength.innerText = 'Strong';
+        strength.innerText = 'strong';
         strength.style.color = 'green';
+        password.style.borderColor = 'green';
     }
 });

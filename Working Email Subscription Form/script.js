c7961c198763855d8c65f638b7c7b7d5e4a12c88
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-08 22:27:45 +0530

    Completed Email subscription using google sheet project

--- Working Email Subscription Form/script.js
+++ Working Email Subscription Form/script.js
@@ -3,22 +3,27 @@
   const scriptURL = 'https://script.google.com/macros/s/AKfycbzVyGbId2I2mCJv_KKW7_BIFTXjYzmiuD2oqPxtCnmnHoFpaJbNPLD56lz6TJ5ZXk8J/exec';
   const form = document.forms['submit-to-google-sheet']
 
-  const input = document.querySelector('input');
-  const span = document.querySelector('span');
+  const msg = document.querySelector('#msg');
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => {
-        console.log('Success!', response)
+        // console.log('Success!', response);
         return response
     })
       .then((response) => {
         if(response.ok){
-            input.value = '';
-            span.style.display = 'block';
+            msg.innerText = 'Thank You for subscribing!';
+            setTimeout(() => {
+                msg.innerText = '';
+            }, 3000);
+            // input.value = '';
+            form.reset(); // This can be used in place of input.value = '';
         }
       })
       .catch(error => console.error('Error!', error.message))
   })
 
+
+//   Thank You for subscribing!

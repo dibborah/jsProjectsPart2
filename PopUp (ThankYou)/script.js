73af49fa8dc9b320db1d11b0a94f3e33cc8f9522
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-08 18:45:33 +0530

    Done pop visibility and hidden functionality(open and close) with transform and top transitions

---
+++ PopUp (ThankYou)/script.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector('.popup');
+
+const openPopup = () => {
+    popup.classList.add('open-popup');
+};
+const closePopup = () => {
+    popup.classList.remove('open-popup');
+};
